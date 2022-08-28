c787582afd6977432a42fe57ff2a0ebff59038bf
everyonelovespepsicola <40620033+everyonelovespepsicola@users.noreply.github.com>
2022-08-27 22:03:28 -0700

    Add files via upload

---
+++ bash/home/fish/functions/fish_prompt.fish
@@ -0,0 +1,11 @@
+function fish_prompt
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+
+    end
+    #echo -n (set_color ff0000)'󰊠  '(set_color ffb5de)'󰊠  '(set_color 00ffde)'󰊠  '(set_color ffb542)'󰊠  '(set_color ffff00)'󰮯  '(set_color ffff00)(prompt_pwd) ''
+   
+    echo -n (set_color ffb542)'󰊠  '(set_color ff0000)'󰊠  '(set_color ffb5de)'󰊠  '(set_color 00ffde)'󰊠  '(set_color ffff00)'󰮯  '(set_color ffff00)(prompt_pwd) ''
+
+    set_color normal
+end
